e31ab675aaad79014c97d6e241793220325d0e38
Jacob Bass <jpbass96@gmail.com>
2026-05-09 13:54:11 -0400

    arm: Add basic semaphore implementation.
    Add a sempahore using the ldaxr and stlxr instructions. The semaphore currently does not work because the MMU
    is not configured. This means all memory accesses are to "device memory" and exclusive monitor instructions
    cause data aborts when performed on device memory regions.

--- include/arm.h
+++ include/arm.h
@@ -1,7 +1,12 @@
 #ifndef _arm_h
 #define _arm_h
-
+#include "types.h"
 #define readmsr(id, __val) asm volatile("mrs %0, "#id : "=r" (__val)); 
+#define writemsr(id, __val) asm volatile("msr " #id ", %0" : : "r"(__val): "memory")
+
+#define DECLARE_ARM64_SEM(_semname) u64 _semame __attribute__((aligned(8)))
+#define INIT_ARM64_SEM(__semname) 
+typedef u64 arm64_sem;
 
 /**
  * struct arm_smccc_res - Result from SMC/HVC call
@@ -14,8 +19,12 @@ struct arm_smccc_res {
         unsigned long a3;
 };
 
-void arm_smccc_smc(unsigned long a0, unsigned long a1, unsigned long a2,
+extern void arm_smccc_smc(unsigned long a0, unsigned long a1, unsigned long a2,
                  unsigned long a3, unsigned long a4, unsigned long a5,
                  unsigned long a6, unsigned long a7, struct arm_smccc_res *res);
+
+extern void arm64_take_semaphore_exclusive(arm64_sem *sem);
+extern void arm64_put_semaphore_exclusive(arm64_sem *sem);
+extern u32 arm64_init_semaphore(arm64_sem *sem);
 
 #endif
